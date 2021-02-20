25174b31c94006f7b24dbc0631ac6798f8547839
Joey Chen <jay.hatelove@gmail.com>
2021-02-20 23:47:33 +0800

    refactor: move duplicated code out of if/else block

--- src/main/java/com/example/budge/homework/BudgetCalculator.java
+++ src/main/java/com/example/budge/homework/BudgetCalculator.java
@@ -42,16 +42,18 @@ public class BudgetCalculator {
             dayCountsEachMonth.put(budgets.get(0).getYearMonth(), end.getDayOfMonth() - start.getDayOfMonth() + 1);
         } else {
             for (int i = 0; i < budgets.size(); i++) {
+                int overlappingDays;
                 if (i == 0) {
-                    int overlappingDays = budgets.get(0).getYearMonthInstance().lengthOfMonth() - start.getDayOfMonth() + 1;
-                    dayCountsEachMonth.put(budgets.get(i).getYearMonth(), overlappingDays);
+                    overlappingDays = budgets.get(0).getYearMonthInstance().lengthOfMonth() - start.getDayOfMonth() + 1;
+//                    dayCountsEachMonth.put(budgets.get(i).getYearMonth(), overlappingDays);
                 } else if (i == budgets.size() - 1) {
-                    int overlappingDays = end.getDayOfMonth();
-                    dayCountsEachMonth.put(budgets.get(i).getYearMonth(), overlappingDays);
+                    overlappingDays = end.getDayOfMonth();
+//                    dayCountsEachMonth.put(budgets.get(i).getYearMonth(), overlappingDays);
                 } else {
-                    int overlappingDays = budgets.get(i).getYearMonthInstance().lengthOfMonth();
-                    dayCountsEachMonth.put(budgets.get(i).getYearMonth(), overlappingDays);
+                    overlappingDays = budgets.get(i).getYearMonthInstance().lengthOfMonth();
+//                    dayCountsEachMonth.put(budgets.get(i).getYearMonth(), overlappingDays);
                 }
+                dayCountsEachMonth.put(budgets.get(i).getYearMonth(), overlappingDays);
             }
         }
 
