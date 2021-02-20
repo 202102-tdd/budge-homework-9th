bec59a44ef5b94ea6b683d6f277b2aed92feeaa5
Joey Chen <jay.hatelove@gmail.com>
2021-02-21 00:18:20 +0800

    refactor: clean up BudgetCalculator

--- src/main/java/com/example/budge/homework/BudgetCalculator.java
+++ src/main/java/com/example/budge/homework/BudgetCalculator.java
@@ -3,21 +3,11 @@ package com.example.budge.homework;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
-import java.time.YearMonth;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-
-import static java.time.format.DateTimeFormatter.*;
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toMap;
 
 @Slf4j
 public class BudgetCalculator {
 
-    private DateTimeFormatter df = ofPattern("yyyyMM");
-    private DateTimeFormatter df2 = ofPattern("yyyyMMdd");
-
-    private BudgetRepo budgetRepo;
+    private final BudgetRepo budgetRepo;
 
     public BudgetCalculator(BudgetRepo budgetRepo) {
         this.budgetRepo = budgetRepo;
